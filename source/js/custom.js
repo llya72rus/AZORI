020f58bb701947a38f6a796e1630091c2dee99b6
llya72rus <32940323+llya72rus@users.noreply.github.com>
2018-05-21 08:53:39 +0300

    Commit 21.05 morning home

--- source/js/custom.js
+++ source/js/custom.js
@@ -262,7 +262,7 @@ $(document).ready(function() {
 
   // Слайдеры
 
-  $('.about__slick').slick({
+  $('.f-about__slick').slick({
     dots: false,
     arrows: true,
     infinite: true,
@@ -365,7 +365,7 @@ $(document).ready(function() {
   });
 
 
-  $('.news__list').slick({
+  $('.news__list:not(.news__list--full)').slick({
     mobileFirst: true,
     arrows: false,
     dots: true,
@@ -378,6 +378,7 @@ $(document).ready(function() {
       }
     ]
   });
+
 
   $('.catalog-top-slider').slick({
     speed: 500,
@@ -392,9 +393,95 @@ $(document).ready(function() {
     slidesToScroll: 1,
     infinite: true,
     arrows: true,
-    nextArrow: '<button type="button" class="collection__gallery-next">Next</button>',
-    prevArrow: '<button type="button" class="collection__gallery-prev">Previous</button>'
+    nextArrow: '<button type="button" class="top-gallery__next">Next</button>',
+    prevArrow: '<button type="button" class="top-gallery__prev">Previous</button>'
   });
+
+  $('.single-new__top-gallery').slick({
+    speed: 500,
+    slidesToShow:  1,
+    slidesToScroll: 1,
+    infinite: false,
+    arrows: true,
+    asNavFor: '.top-gallery__thumbs',
+    nextArrow: '<button type="button" class="top-gallery__next">Next</button>',
+    prevArrow: '<button type="button" class="top-gallery__prev">Previous</button>',
+  });
+
+  $('.top-gallery__thumbs').slick({
+    speed: 500,
+    slidesToShow:  4,
+    slidesToScroll: 4,
+    focusOnSelect: true,
+    infinite: false,
+    animate: false,
+    arrows: false,
+    asNavFor: '.single-new__top-gallery',
+    dots: true,
+
+    responsive: [
+
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+
+      {
+        breakpoint: 670,
+        settings: {
+          slidesToShow: 3,
+        }
+      }
+    ]
+  });
+
+  $(".top-gallery__thumbs .slick-slide").click(function() {
+    $(".top-gallery__thumbs .slick-slide").removeClass('top-gallery__thumb-active');
+    $(this).addClass('top-gallery__thumb-active');
+  });
+
+  // Добавление инлайновых стилей для элементов верхней галлереи на странице отдельной новости
+
+  // (function () {
+  //   var slides = document.querySelector('.single-new__top-gallery .slick-track');
+  //   var singleNewGallery = document.querySelector('.single-new__top-gallery');
+  //   if (slides) {
+  //     if (slides && slides.children.length > 5) {
+  //       singleNewGallery.querySelector('.slick-dots').classList.add('slick-dots-flex-start');
+  //     };
+
+  //     var hasHeightGallery = slides.offsetHeight / 2;
+
+  //     var arrows =  document.querySelectorAll('.single-new__top-gallery .slick-arrow')
+  //     for (var i = 0; i < arrows.length; i++) {
+  //       arrows[i].setAttribute("style", "top: " + hasHeightGallery + "px");
+  //     }
+
+
+  //   if (window.matchMedia("(max-width: 1024px)").matches) {
+  //       if (slides.children.length  > 3) {
+  //         singleNewGallery.querySelector('.slick-dots').classList.add('slick-dots-flex-start');
+  //       };
+  //   }
+
+  //   window.addEventListener("resize", function() {
+  //     if (window.matchMedia("(max-width: 1024px)").matches) {
+  //       if (slides.children.length  > 3) {
+  //         singleNewGallery.querySelector('.slick-dots').classList.add('slick-dots-flex-start');
+  //       };
+  //     }
+  //   });
+  // }
+
+
+
+
+
+  // })();
+
+
 
   $('.coll-examples__slider').slick({
     speed: 500,
@@ -434,11 +521,11 @@ $(document).ready(function() {
     ]
   });
 
-  $('.recommended__wrapper').slick({
+  $('.preview__slider').slick({
     speed: 500,
     slidesToShow:  3,
     slidesToScroll: 1,
-    infinite: true,
+    infinite: false,
     arrows: true,
     nextArrow: '<button type="button" class="orange-rounded-btn-next">Next</button>',
     prevArrow: '<button type="button" class="orange-rounded-btn-prev">Previous</button>',
@@ -510,10 +597,10 @@ $(document).ready(function() {
     speed: 500,
     slidesToShow:  4,
     slidesToScroll: 1,
-    infinite: true,
+    infinite: false,
+    animate: false,
     arrows: false,
     dots: true,
-    focusOnSelect: true,
 
     responsive: [
 
@@ -526,6 +613,11 @@ $(document).ready(function() {
 
       }
     ]
+  });
+
+  $(".collection__colors-list .slick-slide").click(function() {
+    $(".collection__colors-list .slick-slide").removeClass('collection__slick-active');
+    $(this).addClass('collection__slick-active');
   });
 
 
@@ -541,17 +633,23 @@ $(document).ready(function() {
     });
 
 
-    $('.page-nav .menu-toggle').click(function() {
+    $('.page-nav .menu-toggle').click(function(e) {
+      e.stopPropagation();
       $(this).toggleClass('open');
       $('.page-nav').toggleClass('page-nav--menu-opened');
       $('.page-nav__list').toggleClass('page-nav__list--show');
     });
 
-    window.addEventListener('click', function (evt) {
-      if (evt.target === collModal && collModal) {
-        closeModal();
+    $(window).click(function(e) {
+      var target = e.target
+      if($('.page-nav__list').hasClass('page-nav__list--show') && target != $('.page-nav__link')) {
+        $('.page-nav').removeClass('page-nav--menu-opened');
+        $('.page-nav__list').removeClass('page-nav__list--show');
+        $('.menu-toggle').removeClass('open');
       }
     });
+
+
 
   })();
 
@@ -617,6 +715,8 @@ $('.news__desc').each(function() {
   function closeModal () {
     collModal.classList.remove('modal--show');
   }
+
+
 
   var collModal = document.querySelector('.modal');
   var closeCollModalBtn = document.querySelector('.coll-popup__close-btn');
@@ -749,10 +849,6 @@ $('.mobile-search__btn').click(function () {
   //     $(collModal).removeClass('modal--show');
   //   }
   // });
-
-
-  var newsParagraph = $('.news__desc');
-  var newsParagraphHeig
 
 
 
